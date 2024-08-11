d3533b0d3bb7963531d0fb62f9ac76131d99db6e
Akask-Rai-27 <spyeye118@gmail.com>
2024-08-12 00:53:08 +0530

    got the ticket

---
+++ Ticket/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import GetStart from './Component/getStarted/GetStart'
+// import TicketDesign from './Component/Ticket/TicketDesign'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/* <TicketDesign/> */}
+        <GetStart/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Ticket/src/Component/getStarted/GetStart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function GetStart() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default GetStart
